64b03501224c8eaa45b3de8c9ec748dc1fa5f55c
sjs296736196 <296736196@qq.com>
2022-12-11 18:28:37 +0800

    新增依赖antd@5.0.5

--- app-test/src/App.js
+++ app-test/src/App.js
@@ -1,25 +1,32 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import { ConfigProvider, DatePicker, message } from "antd";
+// 由于 antd 组件的默认文案是英文，所以需要修改为中文
+import dayjs from "dayjs";
+import "dayjs/locale/zh-cn";
+import zhCN from "antd/locale/zh_CN";
+import "antd/dist/reset.css";
 import "./App.css";
 
-function App() {
+dayjs.locale("zh-cn");
+
+const App = () => {
+  const [date, setDate] = useState(null);
+  const handleChange = (value) => {
+    message.info(
+      `您选择的日期是: ${value ? value.format("YYYY年MM月DD日") : "未选择"}`
+    );
+    setDate(value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConfigProvider locale={zhCN}>
+      <div style={{ width: 400, margin: "100px auto" }}>
+        <DatePicker onChange={handleChange} />
+        <div style={{ marginTop: 16 }}>
+          当前日期：{date ? date.format("YYYY年MM月DD日") : "未选择"}
+        </div>
+      </div>
+    </ConfigProvider>
   );
-}
+};
 
 export default App;
